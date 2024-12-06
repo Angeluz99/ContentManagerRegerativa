d5ac8030059dadec2d05fc1a7351d2508f7a4c14
regeneratecelulas <regeneratecelulasmadre@gmail.com>
2024-12-06 12:44:56 -0600

    edddveded

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import express from "express";
 import eventosHandler from "./api/eventos";
-import eventosIDHandler from "./api/eventosID"; // Relative path to eventosID.ts
+import eventosIDHandler from "./api/eventosID";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // This middleware is important for handling JSON requests
 
-// Define routes
-app.get("/api/eventos", eventosHandler); // Route for fetching eventos
-app.put("/api/eventos/:id", eventosIDHandler); // Route for updating evento by ID
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+// Default route to check if the server is running
+app.get("/", (req, res) => {
+  res.send("Server is running on Vercel!");
 });
+
+// Define routes
+app.get("/api/eventos", eventosHandler); // Route for fetching events
+app.put("/api/eventos/:id", eventosIDHandler); // Route for updating event by ID
+
+// Vercel expects the app to be exported as a handler for serverless functions
+export default app;
