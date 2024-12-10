f6b445ba1563d4745b5378c4f6bcbccf4a968b50
regeneratecelulas <regeneratecelulasmadre@gmail.com>
2024-12-09 20:55:42 -0600

    working backend

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 import express from "express";
-import eventosHandler from "./api/eventos";
-import eventosIDHandler from "./api/eventosID";
+import cors from "cors"; // Import CORS
+import eventosHandler from "../api/eventos";
+import eventosIDHandler from "../api/eventosID";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); // This middleware is important for handling JSON requests
+app.use(cors()); // Enable CORS for all origins
+app.use(express.json()); // Middleware for handling JSON requests
 
-// Default route to check if the server is running
-app.get("/", (req, res) => {
-  res.send("Server is running on Vercel!");
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
 // Define routes
-app.get("/api/eventos", eventosHandler); // Route for fetching events
-app.put("/api/eventos/:id", eventosIDHandler); // Route for updating event by ID
+app.get("/api/eventos", eventosHandler);
+app.put("/api/eventos/:id", eventosIDHandler);
 
-// Vercel expects the app to be exported as a handler for serverless functions
 export default app;
